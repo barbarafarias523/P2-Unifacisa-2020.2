ca22ad61c0299b2dd12c9ce416c127d70d37ab20
Bárbara Farias <barbaracunha523@gmail.com>
2020-09-18 18:33:25 -0300

    enviando classe da questão 2

---
+++ exercicios/exercicios/Carros.java
@@ -0,0 +1,80 @@
+public class Carros {
+	
+	public enum Cor{
+		BRANCA,PRETA,VERMELHA,AZUL,PRATA,CINZA
+	}
+	
+	String nome, marca;
+	Cor cor;
+	int portas;
+	boolean vidroEletrico, arCondicionado, cambioAutomatico, direcaoEletrica;
+	double precoBase;
+	boolean promocao;
+	
+	 Carros(String nome, String marca, double precoBase) {
+		this.nome = nome;
+		this.marca = marca;
+		this.precoBase = precoBase;
+		this.portas = 2;
+		this.cor = Cor.BRANCA;
+		this.vidroEletrico = false;
+		this.arCondicionado = false;
+		this.cambioAutomatico = false;
+		this.direcaoEletrica = false;
+	}
+	
+	Carros(String nome, String marca, Cor cor, int portas, boolean vidroEletrico, boolean arCondicionado, boolean cambioAutomatico, boolean direcaoEletrica, double precoBase) {
+		this.nome = nome;
+		this.marca = marca;
+		this.cor = cor;
+		this.portas = portas;
+		this.vidroEletrico = vidroEletrico;
+		this.arCondicionado = arCondicionado;
+		this.cambioAutomatico = cambioAutomatico;
+		this.direcaoEletrica = direcaoEletrica;
+		this.precoBase = precoBase;
+	}
+	
+	double calculaPreco() {
+		double preco = precoBase;
+		
+		switch(cor) {
+		case BRANCA:
+		case AZUL:
+		case VERMELHA:
+		case PRETA: break;
+		default: preco +=1000;
+		}
+		
+		if(vidroEletrico == true) {
+			preco+= 1250;
+		}if(arCondicionado == true) {
+			preco += 1250;
+		}if(cambioAutomatico == true) {
+			preco += 1250;
+		}if(direcaoEletrica == true) {
+			preco += 1250;
+		}if(promocao)
+			preco = 0.9 * preco;
+	
+	return preco;
+	
+	
+	}
+
+	public String toString() {
+		String descricao = "";
+		descricao += "Nome do carro: " + this.nome + "\n";
+		descricao += "Marca: " + this.marca + "\n";
+		descricao += "Cor: " + this.cor + "\n";
+		descricao += "Vidros elétricos: " + (this.vidroEletrico ? "sim" : "não") + "\n";
+		descricao += "Ar condicionado: " + (this.arCondicionado ? "sim" : "não") + "\n";
+		descricao += "Cambio Automatico: " + (this.cambioAutomatico ? "sim" : "não") + "\n";
+		descricao += "Direção elétrica: " + (this.direcaoEletrica ? "sim" : "não") + "\n";
+		descricao += "Total: " + calculaPreco() + ", Promoção: " + (promocao ? "sim" : "não") + "\n";
+		return descricao;
+		
+		}
+	
+}
+	
