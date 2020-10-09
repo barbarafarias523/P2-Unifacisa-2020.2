6b04e9e9180e5fd276d6a8d43532efa94b84a182
Bárbara Farias <barbaracunha523@gmail.com>
2020-10-09 15:44:24 -0300

    enviando objeto de string

---
+++ exercicios/MainStringUtils.java
@@ -0,0 +1,15 @@
+public class MainStringUtils {
+
+	public static void main(String[] args) {
+
+		System.out.println(StringUtils.formataAtributo("quantidade de questões"));
+		
+		System.out.println(StringUtils.formataMetodoGet("get quantidade de questões"));
+		
+		System.out.println(StringUtils.formataClasse("formatador de identificadores"));
+		
+		System.out.println(StringUtils.transformaParaCaixaAlta("PhUlaNo dEtHal"));
+
+	}
+
+}
