449b8663460049af402d16e0cf732b89b827a2b3
Bárbara Farias <barbaracunha523@gmail.com>
2020-10-29 18:00:43 -0300

    enviando resolução da atividade

---
+++ exercicios/Sonia.java
@@ -0,0 +1,20 @@
+public class Sonia extends Lutador{
+
+	public Sonia() {
+		super("Sonia");
+	}
+	
+	public void chutar(Lutador l) {
+		if(l != null) {
+			super.chutar(l);
+			l.vida -= 8;
+		}
+	}
+	
+	public void socar(Lutador l) {
+		if(l != null) {
+			super.socar(l);
+			l.vida -= 8;
+		}
+	}
+}
