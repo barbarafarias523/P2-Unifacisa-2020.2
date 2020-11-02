c5cd993e578824a1a0f378240c8ce41500d0d2f0
Bárbara Farias <barbaracunha523@gmail.com>
2020-11-02 19:48:14 -0300

    resolução da questão

---
+++ exercicios/exercicios/Cavalo.java
@@ -0,0 +1,15 @@
+public class Cavalo extends Animal{
+	
+	public Cavalo(String nome, int idade) {
+		super(nome, idade);
+	}
+
+	public void correr() {
+		System.out.println("Cavalo trotando");
+	}
+	
+	public void emitirSom() {
+		System.out.println("Innnnririri");
+	}
+
+}
