13faeff6f7bb31298aea55409a125e10907c9b04
Bárbara Farias <barbaracunha523@gmail.com>
2020-11-02 19:51:04 -0300

    resolução da questão

---
+++ exercicios/exercicios/Preguiça.java
@@ -0,0 +1,15 @@
+public class Preguica extends Animal{
+	
+	public Preguica(String nome, int idade) {
+		super(nome, idade);
+	}
+
+	public void escalarArvore() {
+		System.out.println("Não");
+	}
+	
+	public void emtirSom() {
+		System.out.println("ZZZZzzzzzzzZZZZzzzzzzz");
+	}
+
+}
